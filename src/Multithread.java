569db4d815bbb880043c9e6059774d7aa654c1c7
RoyalPie <truongtk1711@gmail.com>
2025-01-21 14:27:21 +0700

    MultiThread done

--- src/Multithread.java
+++ src/Multithread.java
@@ -2,23 +2,29 @@ import java.util.Iterator;
 import java.util.Random;
 
 public class Multithread {
-        public static void main(String[] args) {
+        public static void main(String[] args) throws InterruptedException {
             int transNum = 10;
-            for(int i=1;i<=transNum;i++){
-                Multi m = new Multi(i);
-                Thread t = new Thread(m);
-                t.start();
+            TransactionSimulation simulation = new TransactionSimulation();
+
+            Thread[] threads = new Thread[transNum];
+
+            long startTime = System.currentTimeMillis();
+            for (int i = 0; i < transNum; i++) {
+                threads[i] = new Thread(new Multi(i+1, simulation));
+                threads[i].start();
             }
-//            long startTime = System.currentTimeMillis();
-//            TransactionSimulation simulation = new TransactionSimulation();
-//            simulation.performTransaction();
-//            long endTime = System.currentTimeMillis();
-//            System.out.println("Final Balance: " + simulation.balance);
-//            System.out.println("Final Balance2: " + (TransactionSimulation.INIT_BALANCE + simulation.credits - simulation.debits));
-//            System.out.println("Total Credits: " + simulation.credits);
-//            System.out.println("Total Debits: " + simulation.debits);
-//            long elapsedTime = endTime - startTime;
-//            System.out.println("Execution Time: " + elapsedTime + " milliseconds");
+
+            for (Thread t : threads) {
+                t.join();
+            }
+
+            long endTime = System.currentTimeMillis();
+            System.out.println("Final Balance: " + simulation.balance);
+            System.out.println("Final Balance2: " + (TransactionSimulation.INIT_BALANCE + simulation.credits - simulation.debits));
+            System.out.println("Total Credits: " + simulation.credits);
+            System.out.println("Total Debits: " + simulation.debits);
+            long elapsedTime = endTime - startTime;
+            System.out.println("Execution Time: " + elapsedTime + " milliseconds");
         }
     }
 
@@ -29,7 +35,7 @@ class TransactionSimulation {
     int credits = 0;
     int debits = 0;
 
-    public void performTransaction() {
+    public synchronized void performTransaction() {
         Random random = new Random();
         for (int i = 0; i < NUM_TRANS; i++) {
             int v = random.nextInt(NUM_TRANS);
@@ -45,24 +51,16 @@ class TransactionSimulation {
         }
     }
 }
-class Multi extends Thread {
+class Multi implements Runnable {
     private int threadNum;
-
-    Multi(int num){
+    private TransactionSimulation simulation;
+    Multi(int num, TransactionSimulation simulation){
         this.threadNum = num;
+        this.simulation = simulation;
     }
     @Override
     public void run() {
         System.out.println("thread "+ threadNum +" is running...");
-        long startTime = System.currentTimeMillis();
-        TransactionSimulation simulation = new TransactionSimulation();
         simulation.performTransaction();
-        long endTime = System.currentTimeMillis();
-        System.out.println("Final Balance: " + simulation.balance);
-        System.out.println("Final Balance2: " + (TransactionSimulation.INIT_BALANCE + simulation.credits - simulation.debits));
-//        System.out.println("Total Credits: " + simulation.credits);
-//        System.out.println("Total Debits: " + simulation.debits);
-        long elapsedTime = endTime - startTime;
-//        System.out.println("Execution Time: " + elapsedTime + " milliseconds");
     }
 }
